116f709c501d28a3f66a8f14014099c8fe82315b
kingdayx <54965488+kingdayx@users.noreply.github.com>
2023-11-04 22:46:25 -0400

    update

--- components/Product.js
+++ components/Product.js
@@ -20,12 +20,12 @@ export default function Product({
   );
   const [hasPrime] = useState(Math.random() < 0.5);
   return (
-    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+    <div className="relative flex flex-col m-10 bg-white z-30 p-10 ">
       <p className="absolute top-2 right-2 text-xs italic text-gray-400">
         {" "}
         {category}{" "}
       </p>
-      <Image className="mx-5" src={image} height={200} width={200} />
+      <Image className="" src={image} height={200} width={200} />
 
       <h4 className="my-3"> {title} </h4>
       <div className="flex">
--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -9,11 +9,38 @@ const getProducts = async () => {
 
 async function ProductFeed() {
   const products = await getProducts();
-
-  console.log("products", products);
   return (
-    <div>
-      {products.map(({ id, title, price, description, category, image }) => {
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto "> 
+      {products.slice(0,4).map(({ id, title, price, description, category, image }) => {
+        return (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        );
+      })}
+      <img className="md:col-span-full" src="https://links.papareact.com/dyz" alt=""/>
+      <div className="md:col-span-2 ">
+      {products.slice(4,5).map(({ id, title, price, description, category, image }) => {
+        return (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        );
+      })}
+      </div>
+      {products.slice(5,products.length).map(({ id, title, price, description, category, image }) => {
         return (
           <Product
             key={id}
